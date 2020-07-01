4737dd406d42760f87975fff5c80bf0f71825881
Ilias Giannoulas <igiannoulas@engineyard.com>
2020-07-01 15:01:07 +0300

    Fixed the deploy hook

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,9 +1,7 @@
 on_app_master() do
-  sudo "monit stop all -g #{config.app}_sidekiq"
   worker_count = 1 # change as needed
   (0...worker_count).each do |i|
-    sudo "monit stop  -g #{config.app}_sidekiq"
-    sleep 10
+    sudo "monit stop  -g #{config.app}_sidekiq && sleep 10"
     sudo "monit start  -g #{config.app}_sidekiq"
   end
 end
