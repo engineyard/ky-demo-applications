0f117b40da79b4015a41b7c245116fa724dcc919
Ilias Giannoulas <igiannoulas@engineyard.com>
2020-07-01 11:46:16 +0300

    Sample sidekiq job #2

--- app/jobs/price_randomizer_job.rb
+++ app/jobs/price_randomizer_job.rb
@@ -2,12 +2,10 @@ class PriceRandomizerJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    h = []
     Spree::Product.all.each do |p| 
-        h << {"product_id" => p.id, "price" => ( (rand Math::E..Math::PI)*rand(50) ).round(2) }
+        p.price = ( (rand Math::E..Math::PI)*rand(50) ).round(2)
+        p.save
     end
-      
-    RandomizePricesWorkerWorker.perform_async(h.to_json, h.count)
       
   end
 end
